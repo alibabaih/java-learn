089e28e6e9c4156b0b848f6c343473e8111760ca
alibabaih <alibabaih@gmail.com>
2014-08-29 17:50:39 +0400

    book-totorials

---
+++ java7-book-examples/src/Stack.java
@@ -0,0 +1,34 @@
+public class Stack {
+
+    public static void main(String[] args){
+
+
+        int stck[] = new int[10];
+        int tos;
+
+        //инициализация верха стека
+        Stack(){
+            tos = -1;
+        }
+
+        void push(int item){
+            if (tos == 9){
+                System.out.println("Стек полон");
+            } else{
+                stck[++tos] = tos;
+            }
+        }
+
+        //выталкивание из стека
+        int pop(){
+            if (tos < 0){
+                System.out.println("Стек не загружен");
+                return 0;
+            } else {
+                return stck[tos--];
+            }
+        }
+
+    }
+
+}
